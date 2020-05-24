524a91f6634f3efeba681ff04e387116544026a5
cricri3007 <christophe.varengo@ens.uvsq.fr>
2020-05-24 23:25:40 +0200

    Create DrawingUI.java

---
+++ src/main/java/DrawingUI.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class DrawingUI {
+
+
+nextCommand (){
+Scanner reader = new Scanner(System.in);  // Reading from System.in
+System.out.println("Que voulez-vous dessiner?: ");
+int n = reader.nextInt(); // Scans the next token of the input as an int.
+reader.close();}
+}
