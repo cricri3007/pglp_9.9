b32258fdc5709014c76d507db01c73be64cd855a
cricri3007 <christophe.varengo@ens.uvsq.fr>
2020-05-24 22:33:07 +0200

    Update Forme.java

--- src/main/java/Forme.java
+++ src/main/java/Forme.java
@@ -1,10 +1,10 @@
 public abstract Forme [
-public int centre.x;
-public int centre.y;
+public int centrex;
+public int centrey;
 
 public void add(Forme forme);
 public void remove(Forme forme);
-public void deplacer(Forme forme);
+public void deplacer(Forme forme, int x, int y);
 public void afficher(Forme forme);
 
 }
