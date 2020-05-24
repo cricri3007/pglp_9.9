dcd242d755e7836715291fbbb9912d691cdcd6f5
cricri3007 <christophe.varengo@ens.uvsq.fr>
2020-05-24 23:13:31 +0200

    Create Cercle.java

---
+++ src/main/java/Cercle.java
@@ -0,0 +1,13 @@
+public class Cercle implements Forme {
+public int centrex;
+public int centrey;
+  public string type;
+  
+public Cercle (int x, int y){
+this.centrex=x;
+this.centrey=y;
+this.type="cercle";
+}
+
+
+}
