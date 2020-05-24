f2b2b7a5f824eb62bb3e72eb88829cc5dbfc48c2
cricri3007 <christophe.varengo@ens.uvsq.fr>
2020-05-24 22:26:09 +0200

    Create Forme.java

---
+++ src/main/java/Forme.java
@@ -0,0 +1,10 @@
+public abstract Forme [
+public int centre.x;
+public int centre.y;
+
+public void add(Forme forme);
+public void remove(Forme forme);
+public void deplacer(Forme forme);
+public void afficher(Forme forme);
+
+}
